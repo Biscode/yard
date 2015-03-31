13321c4843e854daf9f1464a4256c2328b707df3
omar omar <YOUR@EMAIL.com>
2015-03-31 17:59:14 +0200

    user profile #24

--- db/migrate/20150331155540_add_atributes.rb
+++ db/migrate/20150331155540_add_atributes.rb
@@ -1,9 +1,9 @@
-class AddAttributeTocreateUsers < ActiveRecord::Migration
+class AddAtributes < ActiveRecord::Migration
   def change
 	add_column :users, :fname , :string 
 	add_column :users, :lname , :string
 	add_column :users, :birthday , :date 
 	add_column :users, :country , :string 
-	add_column :users, :gender , :inclusion
+	
   end
 end
