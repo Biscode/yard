f3ebe5f7b258b976d9a3530f90ba041446709ee1
AhmedZaleh <ahmedzaleh7@gmail.com>
2015-04-07 00:48:26 +0200

    #14 add some tests for notifications_controller

--- spec/controllers/notifications_controller_spec.rb
+++ spec/controllers/notifications_controller_spec.rb
@@ -7,6 +7,23 @@ RSpec.describe NotificationsController, type: :controller do
       get :index
       expect(response).to have_http_status(:success)
     end
+
+    it "renders the index template" do
+      get :index
+      expect(response).to render_template("index")
+    end
+  end
+
+  describe "Create Notification" do
+    it "creates notification with user1 as the owner and user2 as the notified user" do
+      @user1 = User.create!(email: "ahmedsaleh8@gmail.com", password: "12345678")
+      @user2 = User.create!(email: "Zayd5@gmail.com", password: "12345678")
+      @team = Team.create!(name: "Squad 1")
+
+      @user2.create_activity :create, owner: @user1, parameters: {subject_id: @team.id}
+
+      expect(PublicActivity::Activity.last.owner.email).to eq "ahmedsaleh8@gmail.com"
+    end
   end
 
 end
