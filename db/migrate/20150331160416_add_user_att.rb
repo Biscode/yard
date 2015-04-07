6035a893158ca5849921f98f39d0f29645998b26
omar omar <YOUR@EMAIL.com>
2015-04-07 21:29:20 +0200

    userpage #24

--- db/migrate/20150331160416_add_user_att.rb
+++ db/migrate/20150331160416_add_user_att.rb
@@ -1,6 +1,9 @@
 class AddUserAtt < ActiveRecord::Migration
   def change
-	add_column :users, :birthday , :date 
+  	add_column :users, :fname , :string
+  	add_column :users, :lname , :string
+  	add_column :users, :birthday , :date 
 	add_column :users, :country , :string
+	add_column :users, :profilepicture , :image_tag
   end
 end
