61b77722597cf6eb3a5a8669468bcf52d30ca3d7
martinnabil <martin.nabil7@gmail.com>
2015-04-21 23:29:44 +0200

    retrieving the user model and controller #33

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,35 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
+ include PublicActivity::Model
+  tracked owner: Proc.new { |controller, model| controller.current_user }
+
+  has_many :user_team_relationships
+  has_many :teams,through: :user_team_relationships
+  has_many :tasks
+   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+
+  attr_accessor :password
+
+#for the search in the forum
+def self.search(search)
+  if search
+      find(:all, :conditions => ['name LIKE?', "%#{:search}%"])
+    else
+      find(:all).order("created_at DESC")
+    end
+  end
+
+def self.search(query)
+    # where(:email, query) -> This would return an exact match of the query
+    where("email like ?", "%#{query}%")
+end
+
+## Ahmed Saleh
+## takes a user_id and returns the user email
+def self.find_user(user_id)
+    User.find(user_id).email
+end
 end
