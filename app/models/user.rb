01d6b1a0e6c5ab30ec305b0dcdca222b0a5e2aae
martinnabil <martin.nabil7@gmail.com>
2015-04-21 23:36:48 +0200

    retrieving the user model and controller and update 1  #39

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,18 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  include PublicActivity::Model
+  tracked owner: Proc.new { |controller, model| controller.current_user }
+
+  has_many :user_team_relationships
+  has_many :teams,through: :user_team_relationships
+  has_many :tasks
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :omniauthable, :omniauthable
 attr_accessor :name
 
+
+#for auth with facebook
 def self.find_for_facebook_oauth(auth, signed_in_resource=nil)
     user = User.where(:provider => auth.provider, :uid => auth.uid).first
     if user
@@ -22,7 +30,7 @@ def self.find_for_facebook_oauth(auth, signed_in_resource=nil)
                           )
       end    end
   end
-
+#for auth with titter
 def self.find_for_twitter_oauth(auth, signed_in_resource=nil)
     user = User.where(:provider => auth.provider, :uid => auth.uid).first
     if user
@@ -43,6 +51,7 @@ def self.find_for_twitter_oauth(auth, signed_in_resource=nil)
 
     end
   end
+  #for auth with google
 def self.find_for_google_oauth2(access_token, signed_in_resource=nil)
     data = access_token.info
     user = User.where(:provider => access_token.provider, :uid => access_token.uid ).first
@@ -61,6 +70,25 @@ def self.find_for_google_oauth2(access_token, signed_in_resource=nil)
         )
       end
    end
+end
+#for the search in the forum
+def self.search(search)
+  if search
+      find(:all, :conditions => ['name LIKE?', "%#{:search}%"])
+    else
+      find(:all).order("created_at DESC")
+    end
+  end
+
+def self.search(query)
+    # where(:email, query) -> This would return an exact match of the query
+    where("email like ?", "%#{query}%")
+end
+
+## Ahmed Saleh
+## takes a user_id and returns the user email
+def self.find_user(user_id)
+    User.find(user_id).email
 end
 
 end
