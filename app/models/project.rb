c33a06d6b2bc90b94aeb502950d7f8fb0161412a
AhmedZaleh <ahmedzaleh7@gmail.com>
2015-04-23 01:53:22 +0200

    #44 Add tasks method to Project model to get all tasks in one project.

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,7 +1,13 @@
 class Project < ActiveRecord::Base
-  has_many :tasks
+  # has_many :tasks
   has_many :sprints
   has_many :teams
 
   validates :title, presence: true
+
+  def tasks
+    tasks_array = []
+    tasks_array << sprints.map(&:tasks)
+    tasks_array.flatten
+  end
 end
