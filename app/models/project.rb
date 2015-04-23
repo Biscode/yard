99fb05617b23c6687b3e0ac70e8b89007ba11683
AhmedZaleh <ahmedzaleh7@gmail.com>
2015-04-23 18:30:35 +0200

    Modify and remove unnecessary raltions in models

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,13 +1,13 @@
 class Project < ActiveRecord::Base
-  # has_many :tasks
+  has_many :tasks
   has_many :sprints
   has_many :teams
 
   validates :title, presence: true
 
-  def tasks
-    tasks_array = []
-    tasks_array << sprints.map(&:tasks)
-    tasks_array.flatten
-  end
+  # def tasks
+  #   tasks_array = []
+  #   tasks_array << sprints.map(&:tasks)
+  #   tasks_array.flatten
+  # end
 end
