d37c29da22c5a9ee4922721f80bd3a691bf7c36b
omaromar94 <omaromarmostafa@gmail.com>
2015-04-25 13:25:15 +0300

    making a project id column migration

---
+++ db/migrate/20150425102215_addprojectid.rb
@@ -0,0 +1,4 @@
+class Addprojectid < ActiveRecord::Migration
+  def change
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150425101735) do
+ActiveRecord::Schema.define(version: 20150408204330) do
 
   create_table "activities", force: :cascade do |t|
     t.integer  "trackable_id"
@@ -30,12 +30,6 @@ ActiveRecord::Schema.define(version: 20150425101735) do
   add_index "activities", ["owner_id", "owner_type"], name: "index_activities_on_owner_id_and_owner_type"
   add_index "activities", ["recipient_id", "recipient_type"], name: "index_activities_on_recipient_id_and_recipient_type"
   add_index "activities", ["trackable_id", "trackable_type"], name: "index_activities_on_trackable_id_and_trackable_type"
-
-  create_table "announcements", force: :cascade do |t|
-    t.text     "announcement"
-    t.datetime "created_at",   null: false
-    t.datetime "updated_at",   null: false
-  end
 
   create_table "projects", force: :cascade do |t|
     t.string   "title"
