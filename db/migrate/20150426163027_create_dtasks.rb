1c69147f6ef37c6eeec47a7fcb22954d6851735e
sherif-ihab <sherif.ihab.94@gmail.com>
2015-04-28 08:07:46 +0300

    #32 fixed indentation in dtask migration

--- db/migrate/20150426163027_create_dtasks.rb
+++ db/migrate/20150426163027_create_dtasks.rb
@@ -2,13 +2,11 @@ class CreateDtasks < ActiveRecord::Migration
   def change
     create_table :dtasks do |t|
 
-    	t.references :user	
+      t.references :user	
 
-      
       t.integer :pid
       t.integer :snum
-
-        t.float :story_points
+      t.float :story_points
       
 
       t.timestamps null: false
