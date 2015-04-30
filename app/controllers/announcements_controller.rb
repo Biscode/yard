78ce231b27fce1e444b36b687c630438013d8854
omaromar94 <omaromarmostafa@gmail.com>
2015-04-30 10:31:07 +0300

    fixed the documenatation #14

--- app/controllers/announcements_controller.rb
+++ app/controllers/announcements_controller.rb
@@ -1,66 +1,66 @@
 	class AnnouncementsController < ApplicationController
 	  before_action :set_announcement, only: [:show, :edit, :update, :destroy]
 
-	  # GET /announcements
-	  # GET /announcements.json
-	  def index
-		@project = Project.find(params[:project_id])
-		@announcements = @project.announcements
-	  end
+  # GET /announcements
+  # GET /announcements.json
+  def index
+  @project = Project.find(params[:project_id])
+  @announcements = @project.announcements
+  end
 
-	  # GET /announcements/1
-	  # GET /announcements/1.json
-	  def show
-	  end
+  # GET /announcements/1
+  # GET /announcements/1.json
+  def show
+  end
 
-	  # GET /announcements/new
-	  def new
-		@announcement = Announcement.new
-	  end
+  # GET /announcements/new
+  def new
+  @announcement = Announcement.new
+  end
 
-	  # GET /announcements/1/edit
-	  def edit
-	  end
+  # GET /announcements/1/edit
+  def edit
+  end
 
-	  # POST /announcements
-	  # POST /announcements.json
-	 # Create a new announcement inside a specific project.
-	  def create
-		@project = Project.find(params[:project_id])
-		@announcement = @project.announcements.new(announcement_params)
+  # POST /announcements
+  # POST /announcements.json
+  # Create a new announcement inside a specific project.
+  def create
+  @project = Project.find(params[:project_id])
+  @announcement = @project.announcements.new(announcement_params)
 
-		respond_to do |format|
-		  if @announcement.save
-		    format.html { redirect_to project_path(@project), notice: 'Announcement was successfully created.' }
-		    format.json { render :show, status: :created, location: @announcement }
-		  else
-		    format.html { render :new }
-		    format.json { render json: @announcement.errors, status: :unprocessable_entity }
-		  end
-		end
-	  end
+  respond_to do |format|
+  if @announcement.save
+  format.html { redirect_to project_path(@project), notice: 'Announcement was successfully created.' }
+  format.json { render :show, status: :created, location: @announcement }
+  else
+  format.html { render :new }
+  format.json { render json: @announcement.errors, status: :unprocessable_entity }
+  end
+end
+end
 
-	  # PATCH/PUT /announcements/1
-	  # PATCH/PUT /announcements/1.json
-		 # Update the attributes of the announcement.
+  # PATCH/PUT /announcements/1
+  # PATCH/PUT /announcements/1.json
+		# Update the attributes of the announcement.
 	  def update
-		respond_to do |format|
+	 	respond_to do |format|
 		  if @announcement.update(announcement_params)
 		    format.html { redirect_to @announcement, notice: 'Announcement was successfully updated.' }
 		    format.json { render :show, status: :ok, location: @announcement }
 		  else
 		    format.html { render :edit }
 		    format.json { render json: @announcement.errors, status: :unprocessable_entity }
-		  end
+ end
 		end
 	  end
 
 	  # DELETE /announcements/1
 	  # DELETE /announcements/1.json
-		# Delete an unwanted announcement.
+	 	# Delete an unwanted announcement.
 	  def destroy
-		@announcement.destroy
-		respond_to do |format|
+		  @announcement.destroy
+	 	 respond_to do |format|
 		  format.html { redirect_to announcements_url, notice: 'Announcement was successfully destroyed.' }
 		  format.json { head :no_content }
 		end
