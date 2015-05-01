9c9a59d18d623b9fed701de0f31a5c9fa3ca532a
nohaelshourbagy <nohaelshourbagyy@gmail.com>
2015-05-01 18:49:45 +0300

    fixing conflicts #51

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -1,2 +1,18 @@
 module ProjectsHelper
+	def isAdmin(user)
+	  return user.isAdmin
+	end
+
+	def canView(user, project)
+	  for team in project.teams do
+	    for member in team.users do
+	      if member.email == user.email
+	        return true
+	      end
+	    end
+	  end
+
+	  return false
+
+	end
 end
