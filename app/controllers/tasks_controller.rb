621a0f7f1abb8cb6d9c50ef42a5931b9f88d6877
AhmedZaleh <ahmedzaleh7@gmail.com>
2015-05-02 20:08:11 +0300

    #22 Add Assigning tasks to current users in the project.

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -123,6 +123,6 @@ class TasksController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
   def task_params
-    params.require(:task).permit(:title, :description, :status, :priority, :story_points, :deadline)
+    params.require(:task).permit(:title, :description, :status, :priority, :story_points, :deadline, :user_id)
   end
 end
