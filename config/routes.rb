13e38b4a2549fc2c63990c1c93b946d78bc60620
nohaelshourbagy <nohaelshourbagyy@gmail.com>
2015-05-04 16:04:40 +0300

    conflicts

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,13 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
-  
-
-  
+ 
  
 resources :comments
 resources :tasks do
 resources :comments
 end
-=======
->>>>>>> 5a82131bac7b64ab6648acfb5274a394c7933855
+ resources :announcements
 
-  resources :announcements
 
-<<<<<<< HEAD
-  resources :announcements
-
-=======
 
   
  
@@ -28,7 +19,6 @@ resources :comments
   resources :announcements
 resources :sessions
 devise_for :users, :controllers => { :omniauth_callbacks => "omniauth_callbacks" }
->>>>>>> 9f2c97bc72af4b61744a89ff44e8832931281faf
 root 'projects#index'
 
 
@@ -45,13 +35,10 @@ end
 get 'projects/index'
 #match ':users(/:search(/:))'
 
-<<<<<<< HEAD
 get "log_out" => "sessions#destroy", :as => "log_out"
 get "log_in" => "sessions#new", :as => "log_in"
 get "sign_up" => "users#new", :as => "sign_up"
-=======
 
->>>>>>> 2a4e3ffb73c1609b70fcfec2318d759011c7b955
 #get "users/search"
 resources :teams
 resources :sprints
@@ -60,20 +47,15 @@ resources :notifications
 # resources :tasks
 
 resources :projects do
-<<<<<<< HEAD
+
   resources :tasks
   resources :sprints
-=======
-<<<<<<< HEAD
- resources :announcements
+   resources :announcements
   resources :sprints do
     resources :tasks
   end
-=======
   resources :tasks
   resources :sprints
->>>>>>> 9f2c97bc72af4b61744a89ff44e8832931281faf
->>>>>>> 2a4e3ffb73c1609b70fcfec2318d759011c7b955
   resources :teams do
     resources :users do
       member do
@@ -81,10 +63,7 @@ resources :projects do
       end
     end
   end
-<<<<<<< HEAD
-<<<<<<< HEAD
 root 'projects#index'
-=======
   resources :users
 end
 
@@ -103,11 +82,6 @@ get "sign_up" => "users#new", :as => "sign_up"
   match 'add_task_to_user_task_user' => 'users#add_task_to_user', :as =>'add_task_to_user',via: [:get, :post]
 
 
-
->>>>>>> 5a82131bac7b64ab6648acfb5274a394c7933855
-=======
-
->>>>>>> 2a4e3ffb73c1609b70fcfec2318d759011c7b955
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
